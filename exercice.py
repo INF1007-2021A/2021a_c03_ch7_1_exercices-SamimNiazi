6546f7d53405b0a118e3d6eac953c99d79b71b5e
SamimNiazi <samim.niazi@polymtl.ca>
2021-11-01 23:09:28 -0400

    premier exercise reussi

--- exercice.py
+++ exercice.py
@@ -5,19 +5,30 @@ from collections import deque
 
 
 def get_fibonacci_number(TODO):
-	pass
+	liste = []
+	for i in range (TODO+1):
+		if i == 0:
+			liste.append(0)
+			pass
+		elif i == 1:
+			liste.append(1)
+			pass
+		else:
+			fibonacci_number = liste[i-1] + liste[i-2]
+			liste.append(fibonacci_number)
+	return liste[TODO]
 
-def get_fibonacci_sequence(TODO):
-	pass
+#def get_fibonacci_sequence(TODO):
+	#pass
 
-def get_sorted_dict_by_decimals(TODO):
-	pass
+#def get_sorted_dict_by_decimals(TODO):
+	#pass
 
-def fibonacci_numbers(length):
-	pass
+#def fibonacci_numbers(length):
+	#pass
 
-def build_recursive_sequence_generator(TODO):
-	pass
+#def build_recursive_sequence_generator(TODO):
+	#pass
 
 
 if __name__ == "__main__":
@@ -25,41 +36,41 @@ if __name__ == "__main__":
 	print([get_fibonacci_number(i) for i in range(10)])
 	print()
 
-	print(get_fibonacci_sequence(1))
-	print(get_fibonacci_sequence(2))
-	print(get_fibonacci_sequence(10))
-	print()
+	#print(get_fibonacci_sequence(1))
+	#print(get_fibonacci_sequence(2))
+	#print(get_fibonacci_sequence(10))
+	#print()
 
-	spam = {
-		2: 2.1,
-		3: 3.3,
-		1: 1.4,
-		4: 4.2
-	}
-	eggs = {
-		"foo": 42.6942,
-		"bar": 42.9000,
-		"qux": 69.4269,
-		"yeet": 420.1337
-	}
-	print(get_sorted_dict_by_decimals(spam))
-	print(get_sorted_dict_by_decimals(eggs))
-	print()
+	#spam = {
+		#2: 2.1,
+		#3: 3.3,
+		#1: 1.4,
+		#4: 4.2
+	#}
+	#eggs = {
+		#"foo": 42.6942,
+		#"bar": 42.9000,
+		#"qux": 69.4269,
+		#"yeet": 420.1337
+	#}
+	#print(get_sorted_dict_by_decimals(spam))
+	#print(get_sorted_dict_by_decimals(eggs))
+	#print()
 
-	for fibo_num in fibonacci_numbers(10):
-		print(fibo_num, end=" ")
-	print("\n")
+	#for fibo_num in fibonacci_numbers(10):
+		#print(fibo_num, end=" ")
+	#print("\n")
 
-	def fibo_def(last_elems):
-		return last_elems[-1] + last_elems[-2]
-	fibo = build_recursive_sequence_generator([0, 1], fibo_def)
-	for fi in fibo(10):
-		print(fi, end=" ")
-	print("\n")
+	#def fibo_def(last_elems):
+		#return last_elems[-1] + last_elems[-2]
+	#fibo = build_recursive_sequence_generator([0, 1], fibo_def)
+	#for fi in fibo(10):
+		#print(fi, end=" ")
+	#print("\n")
 
-	lucas = build_recursive_sequence_generator(TODO)
-	print(f"Lucas : {[elem for elem in lucas(10)]}")
-	perrin = build_recursive_sequence_generator(TODO)
-	print(f"Perrin : {[elem for elem in perrin(10)]}")
-	hofstadter_q = build_recursive_sequence_generator(TODO)
-	print(f"Hofstadter-Q : {[elem for elem in hofstadter_q(10)]}")
+	#lucas = build_recursive_sequence_generator(TODO)
+	#print(f"Lucas : {[elem for elem in lucas(10)]}")
+	#perrin = build_recursive_sequence_generator(TODO)
+	#print(f"Perrin : {[elem for elem in perrin(10)]}")
+	#hofstadter_q = build_recursive_sequence_generator(TODO)
+	#print(f"Hofstadter-Q : {[elem for elem in hofstadter_q(10)]}")
